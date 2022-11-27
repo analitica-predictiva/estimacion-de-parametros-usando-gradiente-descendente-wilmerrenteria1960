a1df66cb6782a09e43e2268c3a60facdc4d8587e
Wilmer Renteria cespedes <wilmer@MacBook-Pro-de-Wilmer.local>
2022-11-27 14:04:53 -0500

    update pregunta

--- preguntas.py
+++ preguntas.py
@@ -53,7 +53,7 @@ def pregunta_02():
         error = y - y_pred
 
         # Calcule el gradiente
-        gradient = np.dot(x_poly.T, error)
+        gradient = -1 *np.dot(x_poly.T, error)
 
         # Actualice los parámetros
         params = params - learning_rate * gradient
